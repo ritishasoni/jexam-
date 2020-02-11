e58a9a83a69d4ae064c2ccfe4c87bfd2c713f98d
Joy Mendonca <joymendonca1999@gmail.com>
2020-02-11 12:53:10 +0530

    exam

--- src/main/java/exam/Count.java
+++ src/main/java/exam/Count.java
@@ -7,20 +7,20 @@ public class Count
         int vowels=0,consonants=0;
         char inp[] = str.toCharArray();
 
-        System.out.print("Vowels: ");
         for(int i=0;i<str.length();i++){
             if(inp[i]=='a' || inp[i]=='e' || inp[i]=='i' || inp[i]=='o' || inp[i]=='u')
                 vowels++;
         }
+        System.out.print("Vowels: "+vowels);
 
         //System.out.println("");
 
-        System.out.print("Consonants: ");
         for(int i=0;i<str.length();i++){
             if(inp[i]!='a' && inp[i]!='e' && inp[i]!='i' && inp[i]!='o' && inp[i]!='u')
                 consonants++;
-            System.out.println("Consonants="+consonants+"\nVowels="+vowels);
+
         }
+        System.out.print("Consonants: "+consonants);
     }
 
 
